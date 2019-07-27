225068ba92e939ac55b445cdbfeae998c6a04ac9
Marit Iren <marit.iren.92@gmail.com>
2019-07-28 01:44:17 +0200

    Add vuetify and light theme. Test background coloring.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <v-app>
+    <v-app style="
+  background: #fcd277;
+  background: -webkit-linear-gradient(to right, #fcd277, #73b1f4, #b4e080, #fcd277);
+  background: linear-gradient(to right, #fcd277 60%, #73b1f4 60%, #73b1f4 70%, #ec5564 70%, #ec5564 80%, #b4e080 80%, #b4e080 90%, #fcd277 90%);
+    ">
     <Menu/>
 
     <v-content>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
-      <Home/>
       <Home/>
     </v-content>
   </v-app>
---
+++ src/themes/light.js
@@ -0,0 +1,7 @@
+export default {
+    primary: "#ffffff", // Sunflower #fecd57 #f5ba45
+    sunflower: "#fcd277", // Sunflower #fecd57 #f5ba45
+    bluejeans: "#73b1f4", // blue jeans #5e9cea #4b89da
+    grass: "#b4e080", // grass #9ed36a #8aco54
+    grapefruit: "#ec5564" // grapefruit #f76d82 #ec5564 #d94452
+};
