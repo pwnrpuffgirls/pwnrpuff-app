762597fb701e20cd00fbb2b740938fb605ee8712
Marit Iren <marit.iren.92@gmail.com>
2019-07-28 15:20:10 +0200

    Change CSS background.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-    <v-app style="
-  background: #fcd277;
-  background: -webkit-linear-gradient(to right, #fcd277, #73b1f4, #b4e080, #fcd277);
-  background: linear-gradient(to right, #fcd277 60%, #73b1f4 60%, #73b1f4 70%, #ec5564 70%, #ec5564 80%, #b4e080 80%, #b4e080 90%, #fcd277 90%);
+    <v-app dark style="
+        background: -webkit-linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), -webkit-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
+    background: -o-linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), -o-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
+    background: -moz-linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), -moz-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
+    background: linear-gradient(70deg, #ec5564 30%, rgba(0,0,0,0) 30%), linear-gradient(30deg, #63e89e 60%, #73b1f4 60%);
     ">
     <Menu/>
 
@@ -27,3 +28,10 @@ export default {
   }),
 };
 </script>
+//.bg-purple {
+//color: #fff;
+//background: -webkit-linear-gradient(110deg, #a60af3 40%, rgba(0, 0, 0, 0) 30%), -webkit-radial-gradient(farthest-corner at 0% 0%, #7a00cc 70%, #c03fff 70%);
+//background: -o-linear-gradient(110deg, #a60af3 40%, rgba(0, 0, 0, 0) 30%), -o-radial-gradient(farthest-corner at 0% 0%, #7a00cc 70%, #c03fff 70%);
+//background: -moz-linear-gradient(110deg, #a60af3 40%, rgba(0, 0, 0, 0) 30%), -moz-radial-gradient(farthest-corner at 0% 0%, #7a00cc 70%, #c03fff 70%);
+//background: linear-gradient(110deg, #fcd277 40%, rgba(0, 0, 0, 0) 30%), radial-gradient(farthest-corner at 0% 0%, #fecd57 70%, #f5ba45 70%);
+//}
