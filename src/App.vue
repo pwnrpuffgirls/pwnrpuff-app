04b808e0f86781513a2a5c44a04b1304b553b124
Marit Iren <marit.iren.92@gmail.com>
2019-08-17 23:22:56 +0200

    Remove CSS background

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
 
-  <v-app dark
-         style="
-          background: -webkit-linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), -webkit-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
-      background: -o-linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), -o-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
-      background: -moz-linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), -moz-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
-      background: linear-gradient(70deg, #ec5564 30%, rgba(0,0,0,0) 30%), linear-gradient(30deg, #63e89e 60%, #73b1f4 60%);
-      ">
+  <v-app dark>
     <SemiStickyHeader/>
     <v-content class="content">
       <router-view/>
