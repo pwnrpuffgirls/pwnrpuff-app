b631f107e39a5af226a9e8a6373604269c6696eb
Marit Iren <marit.iren.92@gmail.com>
2019-08-24 18:58:38 +0200

    Add contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,14 @@
 <template>
-    $END$
+    <div>
+        <h1>Contact</h1>
+        <p>
+            Have any questions, or want to join our infosec community
+            for girls and non-binary people? Send us an email!
+        </p>
+        <p>
+            pwnrpuffgirls@gmail.com
+        </p>
+    </div>
 </template>
 
 <script>
